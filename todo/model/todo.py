5027a69f3a11b1e33faca5011e1c02c9897f7b86
AARON HENAO <aaronhenao27@gmail.com>
2023-02-27 10:03:10 -0500

    Primer punto taller

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,4 +1,11 @@
 class Todo:
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = False
+        self.tags = tags = []
+
     pass
 
 
